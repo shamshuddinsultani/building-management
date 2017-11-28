2a33b23178a1fe34e7c357a892c8fd8a56679f37
shamshuddinsultani <khan.shamshuddin2000@gmail.com>
2017-11-28 16:51:35 +0530

    data fetched table order

--- reports.php
+++ reports.php
@@ -14,18 +14,23 @@ header('location:index.php');
                 	<div class="col-sm-12 col-xs-12">
                 		<fieldset>
                 			<legend>Reports</legend>
-                			<!-- Large modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
-
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-     <?php exportMembersData(); ?>
-    </div>
-  </div>
-</div>
+                		<ul>
+                			<li>
+<a href="javascript:void(0);" onclick="window.open('members_data.php', '_blank', 'width=800,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=0,screeny=0');" title="View all closed community complaints">Members Data</a></li>
+</ul>
                 		</fieldset>
                 	</div>
                 	</div>
                 </div>
 <?php include 'footer.php'; ?>
+
+
+<!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+     <?php/* exportMembersData();*/ ?>
+    </div>
+  </div>
+</div>-->
