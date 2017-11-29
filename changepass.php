dea423ed7ed7c1cc7fe78669f3bc7b688651f072
shamshuddinsultani <khan.shamshuddin2000@gmail.com>
2017-11-29 16:43:39 +0530

    beautified

--- changepass.php
+++ changepass.php
@@ -9,31 +9,31 @@ header('location:index.php');
 <?php if(isset($_POST['submit'])){
   changePassword();
 }?>
-        <div id="page-wrapper">
-        	     <div id="page-inner">
-              <div class="row">
-              	<fieldset>
-              		<legend>Change Password</legend>
-              		<form action="" method="post">
-              	<table class="table table-dark">
-  <tbody>
-    <tr>
-      <td>Old Pass:</td>
-      <td><input type="text" name="oldpass"></td>
-     </tr>
-     <tr>
-      <td>New Pass:</td>
-      <td><input type="text" name="newpass"></td>
-     </tr>
-     <tr>
-      <td>Con Pass:</td>
-      <td><input type="text" name="conpass"></td>
-     </tr>
-  </tbody>
+<div id="page-wrapper">
+<div id="page-inner">
+<div class="row">
+<fieldset>
+<legend>Change Password</legend>
+<form action="" method="post">
+<table class="table table-dark">
+<tbody>
+<tr>
+    <td>Old Pass:</td>
+    <td><input type="text" name="oldpass"></td>
+</tr>
+<tr>
+    <td>New Pass:</td>
+    <td><input type="text" name="newpass"></td>
+</tr>
+<tr>
+    <td>Con Pass:</td>
+    <td><input type="text" name="conpass"></td>
+</tr>
+</tbody>
 </table>
-                <button type="submit" name="submit" class="btn btn-info">Change</button>
-              </form> 
-              </fieldset>
-              </div>
-          </div>
+<button type="submit" name="submit" class="btn btn-info">Change</button>
+</form>
+</fieldset>
+</div>
+</div>
 <?php include 'footer.php'; ?>
